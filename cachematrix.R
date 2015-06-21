3d4b9ec3b96b563668ea29109e0af2d4f0d6d03f
sjjbryant <sjjbryant@gmail.com>
2015-06-21 15:40:04 -0700

    real homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## matrix inversion is a costly computation so having a means to cache an 
+## inversion rather than recalculate it is valuable. These two functions 
+## work together to invert a matrix and save it outside the function so their
+## values can be fetched instead of recalculated if the original exists
 
-## Write a short comment describing this function
+## This is a function that creates and stores a list of functions
+## by using the <<- operator to assign variables to a separate environment
+## It returns a list of 4 getting and setting functions in a separate environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
-
+        m <- NULL
+        ## sets original matrix
+        setmatx <- function(y) {
+                ## create variables in separate environment
+                x <<- y
+                ## set to null so we know it has not been calc'd already
+                m <<- NULL
+        }
+        getmatx <- function() x
+        setinverse <- function(matx) m <<- matx
+        getinverse <- function() m
+        ## return list of functions
+        list(setmatx = setmatx, getmatx = getmatx,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function uses the functions created in makeCacheMatrix() to determine if it should
+## compute the inverse of the matrix or return the cached version if it already exists.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    
+        
+        m <- x$getinverse()
+        ## see if inverted matrix m has been calc'd before and return it if it has
+        if(!is.null(m)) {
+                return(m)
+        }
+        ## matrix not already calc'd, so get it, invert it, and return it
+        matricks <- x$getmatx()
+        m <- solve(matricks, ...)
+        x$setinverse(m)
+        m
 }
